649aea03ec1da698732d58143718c0525049ace0
96654 <96654@AlAnood>
2022-04-24 16:56:37 +0300

    ..

---
+++ OOPProject/src/User.java
@@ -0,0 +1,86 @@
+import java.time.LocalDate;
+import java.util.*;
+public class User extends Person {
+	//data fields 
+	private String  username;
+    private String  password;
+    private LocalDate dateCreated;
+    private static int AccountNumber = 0;
+    private ArrayList <UserRating> rate = new ArrayList<UserRating>();
+    private ArrayList <UserComment> comment = new ArrayList<UserComment>();
+    
+    //constructors
+    public User(){
+    	dateCreated = LocalDate.now();
+    	AccountNumber += 1;
+    }
+    public User(String username, String password){
+    	try{
+	    	setUsername(username);
+	    	setPassword(password);
+    	}
+    	catch(Exception e){
+    		System.out.println(e);
+    	}
+    }
+    
+	public User(String name, String email, String mobile, Date birthDate , String username, String password){
+    	super(name,email,mobile,birthDate);
+    	try{
+	    	setUsername(username);
+	    	setPassword(password);
+    	}
+    	catch(Exception e){
+    		System.out.println(e);
+    	}
+    	
+    }
+    //Getters and setters
+    public String getUsername() {
+    	return username;
+    }
+
+    public void setUsername(String username) throws Exception {
+    	boolean checkUserName = username.matches("[a-zA-Z0-9]{6,15}");
+    	if(!checkUserName){
+    		throw new Exception("username must only contains alphanumeric characters for minimum 6 digits");
+    	}
+    	else
+    	    this.username = username;
+    }
+
+	public String getPassword() {
+	    return password;
+    }
+	
+	public void setPassword(String password) throws Exception{
+		boolean checkPassword = password.matches("[a-zA-Z0-9]{8,}");
+		if(!checkPassword){
+			throw new Exception("password must only contains alphanumeric characters for minimum 6 digits");
+		}
+		else
+	        this.password = password;
+	}
+	
+	public int getAccountNumber(){
+		return AccountNumber;
+	}
+
+	public LocalDate getDateCreated() {
+		return dateCreated;
+	}
+	
+    public void Rate(String movieID,double ratingValue){
+    	rate.add(new UserRating(movieID,ratingValue,username));
+    }
+    
+    public void Comment(String movieID,String comment){
+    	this.comment.add(new UserComment(movieID,comment,username));
+    }
+ 
+    public String toString() {
+        return super.toString()+"\nusername: "+ username + "\nPassword: " + password ;
+  }
+
+
+}
